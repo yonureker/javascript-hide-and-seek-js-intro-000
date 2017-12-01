128ac688261b134018b2db03744540da6c4679db
Onur Eker <yonureker@gmail.com>
2017-12-01 22:25:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector){
+	return document.querySelector(selector)
+}
+
+function nestedTarget(){
+	return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('ul.ranked-list');
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = lis[i] + n
+}
+}
+
+function deepestChild(){
+  var nodes = document.getElementById('grand-node').querySelectorAll('div');
+  var deepElem = nodes[nodes.length - 1];
+  return deepElem
+}
