c850101b25428a7ae31f3b7b9262796d5aa177bc
Onur Eker <yonureker@gmail.com>
2017-12-01 22:25:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n){
   const lis = document.querySelectorAll('ul.ranked-list');
 
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (i + n).toString()
+    lis[i].innerHTML = lis[i] + n
 }
 }
 
