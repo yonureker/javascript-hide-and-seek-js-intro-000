112a2b4f89c04cd30815ad359a4ce981ae89f76b
Onur Eker <yonureker@gmail.com>
2017-12-01 22:02:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  const deeps = document.getElementById('grand-node').querySelectorAll('div div div div div:firstChild');;
+  return document.querySelector('#grand-node div div div div');
 }
